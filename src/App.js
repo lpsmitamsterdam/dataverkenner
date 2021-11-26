ca39018e48c06a78589c714fd377eeb37d472bf6
Leonard Smit <l.p.smit@amsterdam.nl>
2021-11-26 12:09:30 +0100

    add dockerfile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
         <img alt="amsterdam-logo" src="https://www.amsterdam.nl/views/core/images/logos/andreas.png" />
         <p>
-          Welkom bij de dataverkenner 3.0
+          Welkom bij de dataverkenner 3.0, deze app draait in een container
         </p>
           <a style={{color: "red"}} href="https://data.amsterdam.nl">Ga naar de oude omgeving</a>
       </header>
